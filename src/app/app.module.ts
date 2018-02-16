de392b39ffb0ea8fe1f83338dff6208bac194658
Nabil5352 <nabil.ibn.mahmud@gmail.com>
2018-02-17 01:08:15 +0600

    to-do-app initial push

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent,
+    TaskDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,16 @@
+import { Task } from './task';
+
+export const TASKS: Task[] = [
+	{ id: 1, title: 'Wake Up' },
+	{ id: 2, title: 'Exercise' },
+	{ id: 3, title: 'Brush Teeth' },
+	{ id: 4, title: 'Take Shower' },
+	{ id: 5, title: 'Eat Breakfast' },
+	{ id: 6, title: 'Go to Work' },
+	{ id: 7, title: 'Have Lunch' },
+	{ id: 8, title: 'Back from Work' },
+	{ id: 9, title: 'Read' },
+	{ id: 10, title: 'Working on a Personal Project' },
+	{ id: 11, title: 'Browsing' },
+	{ id: 12, title: 'Go to Sleep' }
+];
